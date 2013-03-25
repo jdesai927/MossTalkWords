c8c68b7c12cf1252b0c9e9c85d912d00ee7b09bc
Michael Posner <posnerml@seas.upenn.edu>
2013-03-25 18:57:41 -0400

    modified EndSet layout, started updating MainActivityTest

--- MossTalkWordsTestTest/src/edu/upenn/cis350/mosstalkwords/test/MainActivityTest.java
+++ MossTalkWordsTestTest/src/edu/upenn/cis350/mosstalkwords/test/MainActivityTest.java
@@ -2,9 +2,7 @@ package edu.upenn.cis350.mosstalkwords.test;
 
 import java.util.ArrayList;
 
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.speech.RecognizerIntent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.widget.Button;
 import edu.upenn.cis350.mosstalkwords.MainActivity;
@@ -28,14 +26,25 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 		//  (this substitutes for what PickSet would normally pass to MainActivity)
 		Intent i = new Intent();
 		i.putExtra(PickSet.currentSetPath, "nonlivingthingshard");
-		i.putExtra(PickSet.currentSet, 
-				getInstrumentation().getTargetContext().getApplicationContext().getResources().getStringArray(
-						edu.upenn.cis350.mosstalkwords.R.array.nonlivingthingshard));
+		
+		ArrayList<String> curr_set = new ArrayList<String>();
+		curr_set.add("parachute");
+		curr_set.add("freezer");
+		curr_set.add("marker");
+		curr_set.add("icicle");
+		curr_set.add("boomerang");
+		curr_set.add("calculator");
+		curr_set.add("stadium");
+		curr_set.add("motorcycle");
+		curr_set.add("toothbrush");
+		curr_set.add("spatula");
+		i.putStringArrayListExtra(PickSet.currentSet,curr_set);
 		
 		setActivityIntent(i);  //later MainActivity calls to getIntent will return this (we need this to 
 								// initialize set directories, etc
 
 		act = (MainActivity) this.getActivity();
+		
 		
 		hintPhraseButton = (Button) act.findViewById(R.id.hintbuttona);
         hintRhymeButton = (Button) act.findViewById(R.id.hintbuttonb);
@@ -114,34 +123,7 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
 			
 		assertEquals("hints used after skipping", 0, act._numHintsUsed);
 	}
-	
-	//VOICE TEST DOESNT WORK RIGHT NOW BECAUSE I CANT SEEM TO GET THE ALERT DIALOG STARTING
-	//  AND CLICKING TO WORK WHEN TESTING, SO SCORE ISNT UPDATED
-//	/**
-//	 * Test what happens after a correct voice recognition result by simulating the
-//	 * voice recognition's resulting intent and passing it to MainActivity.onActivityResult()
-//	 */
-//	public void testVoiceCorrect() {
-//		Intent i = new Intent();
-//		ArrayList<String> voice_results = new ArrayList<String>();
-//		voice_results.add("freezer");
-//		
-//		i.putStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS, voice_results);
-//		
-//		act.onActivityResult(1, act.RESULT_OK, i);
-//		
-//		getInstrumentation().waitForIdleSync();  //wait for the AlertDialog to pop up
-//		
-//		act.runOnUiThread(new Runnable() {
-//			public void run() {
-//				act.ad.getButton(DialogInterface.BUTTON_POSITIVE).performClick();
-//			}
-//		});
-//		
-//		getInstrumentation().waitForIdleSync();  //wait for the app to process a "continue" click
-//		
-//		assertEquals("score updated for correct voice result",3,act._score);
-//	}
+
 	
 	
 	
